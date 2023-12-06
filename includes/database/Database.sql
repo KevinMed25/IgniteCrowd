6655c6b9efdffd7425f97744be3a19d013fc9adb
Kevin Medina <kevinmedina2509Jr@gmail.com>
2023-12-06 14:23:41 -0600

    fix: actualizacion a db

--- includes/database/Database.sql
+++ includes/database/Database.sql
@@ -16,36 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `ingresos`
---
-
-DROP TABLE IF EXISTS `ingresos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ingresos` (
-  `id` int NOT NULL,
-  `idusuario` int NOT NULL,
-  `idproyectos` int NOT NULL,
-  `ingresos` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idusuario` (`idusuario`) /*!80000 INVISIBLE */,
-  KEY `idproyectos_idx` (`idproyectos`),
-  CONSTRAINT `idproyectos` FOREIGN KEY (`idproyectos`) REFERENCES `proyectos` (`id`),
-  CONSTRAINT `idusuario` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ingresos`
---
-
-LOCK TABLES `ingresos` WRITE;
-/*!40000 ALTER TABLE `ingresos` DISABLE KEYS */;
-INSERT INTO `ingresos` VALUES (1,1,1,149.00),(2,1,2,300.00),(3,1,3,365.00);
-/*!40000 ALTER TABLE `ingresos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `proyectos`
 --
 
@@ -63,8 +33,12 @@ CREATE TABLE `proyectos` (
   `categoria` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `ciudad` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `fecha_creacion` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `ingresos` decimal(10,2) DEFAULT NULL,
+  `usuarioid` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuarioid_idx` (`usuarioid`),
+  CONSTRAINT `usuarioid` FOREIGN KEY (`usuarioid`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +47,7 @@ CREATE TABLE `proyectos` (
 
 LOCK TABLES `proyectos` WRITE;
 /*!40000 ALTER TABLE `proyectos` DISABLE KEYS */;
-INSERT INTO `proyectos` VALUES (1,'Infierno',' Aquí irá una breve descripcion general del proyecto',700.00,169,'InfernoSmall.jpg',NULL,'Libros','Mérida, Yucatán','2023-12-02'),(2,'Electro Albúm',' Aquí irá una breve descripcion general del proyecto',800.00,400,'AlumMusica.jpeg',NULL,'Música','Mérida, Yucatán','2023-12-02'),(3,'Tokyo Ghoul',' Aquí irá una breve descripcion general del proyecto',400.00,320,'portadaManga.jpg',NULL,'Mangas','Mérida, Yucatán','2023-12-02');
+INSERT INTO `proyectos` VALUES (1,'Infierno',' Aquí irá una breve descripcion general del proyecto',700.00,169,'InfernoSmall.jpg',NULL,'Libros','Mérida, Yucatán','2023-12-02',149.00,1),(2,'Electro Albúm',' Aquí irá una breve descripcion general del proyecto',800.00,400,'AlumMusica.jpeg',NULL,'Música','Mérida, Yucatán','2023-12-02',300.00,1),(3,'Tokyo Ghoul',' Aquí irá una breve descripcion general del proyecto',400.00,320,'portadaManga.jpg',NULL,'Mangas','Mérida, Yucatán','2023-12-02',365.00,1),(7,' Berserk','Es un manga creado por Kentaro Miura y posteriormente adaptado en anime, con un estilo épico fantástico y de fantasía oscura. El manga comenzó a publicarse en 1989 en la extinta revista mensual Monthly Animal House, hasta que fue reemplazada en 1992 por la revista quincenal Young Animal, donde fue publicado irregularmente hasta el 2021 tras la muerte de su autor',1000.00,0,'8b122a5dd7eba233608f35e11713d233.jpg',NULL,'categoria2','Mérida, Yucatán','2023-12-06',0.00,1);
 /*!40000 ALTER TABLE `proyectos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +68,7 @@ CREATE TABLE `usuarios` (
   `telefono` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `crowdfunder` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +77,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Kevin','Medina Padilla',NULL,'$2y$10$fIlI6uPUS8B49XEyqwEuJOcu/cmuVAiukMAZjEpDXRo5h8yKm8TWm','kevinmedina2509Jr@gmail.com','9996131871',0);
+INSERT INTO `usuarios` VALUES (1,'Kevin','Medina Padilla',NULL,'$2y$10$fIlI6uPUS8B49XEyqwEuJOcu/cmuVAiukMAZjEpDXRo5h8yKm8TWm','kevinmedina2509Jr@gmail.com','9996131871',0),(2,' Kevin ','Medina','Padilla','$2y$10$0A/zkHI9EmnZcwppuS57muACaWNFb6WyPVhHY3UFgDA6gIwaXBv4e','correo@correo.com','9996131871',0);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -116,4 +90,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-05 23:29:40
+-- Dump completed on 2023-12-06 14:23:00
